9e48f12f1ec4ce8c9c889aebfec885a4433efda8
Andre Fonseca <decobf15@gmail.com>
2024-05-02 15:24:01 -0300

    projeto finalizado

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+      Scanner sc = new Scanner(System.in);
+
+      System.out.println("Por favor digite o número da agencia: ");
+      int numero = sc.nextInt();
+      sc.nextLine();
+      System.out.println("Informe a agencia: ");
+      String agencia = sc.nextLine();
+      System.out.println("Informe o nome do cliente: ");
+      String nomeCliente = sc.nextLine();
+      System.out.println("Informe o saldo: ");
+      double saldo = sc.nextDouble();
+
+      System.out.println("Olá " + nomeCliente + ", obirgado por criar uma conta em nosso banco, sua agência é " + agencia
+      + " conta " + numero + " e seu saldo " + saldo + " já está disponivel para saque");
+
+      sc.close();
     }
 }
